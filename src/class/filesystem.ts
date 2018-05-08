c185cb906f8f96676960d4b7508fba3aad4b99f0
kdcro101 <kdcro101.github@gmail.com>
2018-05-08 21:58:51 +0200

    filesystem

--- src/class/filesystem.ts
+++ src/class/filesystem.ts
@@ -21,6 +21,8 @@ export class FilesystemUtils {
                 }
                 if (val === base) {
                     vscode.window.showWarningMessage("New name must be different");
+                    reject("not_different");
+                    return;
                 }
                 const newBase = val.trim();
                 const newPath = path.join(dir, newBase);
@@ -33,6 +35,67 @@ export class FilesystemUtils {
                         vscode.window.showErrorMessage(`Error duplicating ${base}`);
                         reject(e);
                     });
+
+            });
+        });
+    }
+    public delete(item: ViewItem) {
+        return new Promise((resolve, reject) => {
+            const aPath = item.value;
+            const base = path.basename(aPath);
+            const dir = path.dirname(aPath);
+            vscode.window.showQuickPick(
+                ["Yes", "No"], {
+                    placeHolder: `Are you sure you want to delete '${base}'`,
+                },
+            ).then((val) => {
+                if (val == null || val.toLocaleLowerCase() === "no") {
+                    resolve();
+                    return;
+                }
+
+                fs.remove(aPath)
+                    .then((result) => {
+                        resolve();
+                    })
+                    .catch((e) => {
+                        vscode.window.showErrorMessage(`Error deleting ${base}`);
+                        reject(e);
+                    });
+
+            });
+        });
+    }
+
+    public rename(item: ViewItem) {
+        return new Promise((resolve, reject) => {
+            const aPath = item.value;
+            const base = path.basename(aPath);
+            const dir = path.dirname(aPath);
+            vscode.window.showInputBox({
+                prompt: "Enter new name",
+                placeHolder: "Type new name",
+                value: base,
+            }).then((val) => {
+                if (val == null || val.trim() === "") {
+                    return;
+                }
+                if (val === base) {
+                    vscode.window.showWarningMessage("New name must be different");
+                    reject("not_different");
+                    return;
+                }
+                const newBase = val.trim();
+                const newPath = path.join(dir, newBase);
+                fs.move(aPath, newPath, {
+                    overwrite: true,
+                }).then((result) => {
+                    vscode.window.showInformationMessage(`Duplication successful`);
+                    resolve();
+                }).catch((e) => {
+                    vscode.window.showErrorMessage(`Error duplicating ${base}`);
+                    reject(e);
+                });
 
             });
         });
